4f012c7d9aa1547348035bb8d05048fd405e91c5
wsmith42 <wsmith42@studentmail.nait.ca>
2017-01-30 16:50:01 -0700

    Creation of project and website

---
+++ DummySolution1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DummySolution.Startup))]
+namespace DummySolution
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
